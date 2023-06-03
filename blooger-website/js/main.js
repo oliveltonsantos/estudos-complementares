85f2abcb19a84f26c31d4af5145799de7617dcdf
Olivelton Santos <124373744+oliveltonsantos@users.noreply.github.com>
2023-06-03 13:04:18 -0300

    update

--- blooger-website/js/main.js
+++ blooger-website/js/main.js
@@ -9,7 +9,14 @@ $(document).ready(function () {
     })
 
     // owl-crousel for blog
-    $('.owl-carousel').owlCarousel();
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        autoplay: false,
+        autoplayTimeout: 3000,
+        dots: false,
+        nav: true,
+        navText: [$('.owl-navigation .owl-nav-prev'),$('.owl-navigation .owl-nav-next')],
+    });
 
     
 });
