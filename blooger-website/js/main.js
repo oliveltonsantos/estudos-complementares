67e11c6487ab4adaabf0321b0e44aa775d203b3b
Olivelton Santos <124373744+oliveltonsantos@users.noreply.github.com>
2023-06-06 12:31:46 -0300

    update

--- blooger-website/js/main.js
+++ blooger-website/js/main.js
@@ -1,3 +1,19 @@
+const responsive = {
+    0: {
+        items: 1
+    },
+    320: {
+        items: 1
+    },
+    560: {
+        items: 2
+    },
+    960: {
+        items: 3
+    }
+}
+
+
 $(document).ready(function () {
 
     $nav = $('.nav');
@@ -16,7 +32,19 @@ $(document).ready(function () {
         dots: false,
         nav: true,
         navText: [$('.owl-navigation .owl-nav-prev'),$('.owl-navigation .owl-nav-next')],
+        responsive: responsive
     });
+
+    // click to scroll top
+    $('.move-up span').click(function () {
+        $('html, body').animate({
+            scrollTop: 0
+        }, 1000);
+    })
+
+    // AOS Instance
+    AOS.init(); 
+
 
     
 });
