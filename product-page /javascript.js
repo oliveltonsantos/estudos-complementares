fd944b670b7a09d11f7784467accfd261ab0d9b4
Olivelton Santos <124373744+oliveltonsantos@users.noreply.github.com>
2023-07-07 11:25:25 -0300

    update

---
+++ product-page /javascript.js
@@ -0,0 +1,32 @@
+let productImg = document.getElementById('productImg')
+
+let btn = document.getElementsByClassName('btn')
+
+btn[0].onclick = function() {
+    productImg.src = 'images/image1.png'
+
+    for(bt of btn) {
+        bt.classList.remove('active')
+    }
+    this.classList.add('active')
+}
+
+btn[1].onclick = function() {
+    productImg.src = 'images/image2.png'
+
+    for(bt of btn) {
+        bt.classList.remove('active')
+    }
+    this.classList.add('active')
+}
+
+btn[2].onclick = function() {
+    productImg.src = 'images/image3.png'
+
+    for(bt of btn) {
+        bt.classList.remove('active')
+    }
+    this.classList.add('active')
+}
+
+
