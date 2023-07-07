93e325c6710b7f4b7b07f7c8e377d5886d0c041b
Olivelton Santos <124373744+oliveltonsantos@users.noreply.github.com>
2023-07-07 00:26:21 -0300

    update

--- weather-app/javascript.js
+++ weather-app/javascript.js
@@ -1,30 +1,53 @@
-const apiKey = '863242cfb2b1d357e6093d9a4df19a4b'
-
-const apiUrl = 'https://api.openweather.org/data/2.5/weather?units=metric&q='
+const apiKey = '80f1511f3ad38d4c2eb83182de654655'
+const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?units=metric&q='
 
 const searchBox = document.querySelector('.search input')
-
 const searchBtn = document.querySelector('.search button')
+const weatherIcon = document.querySelector('.weather-icon')
 
 async function checkWeather(city) {
     const response = await fetch(apiUrl + city + `&appid=${apiKey}`)
-    var data = await response.json()
 
-    console.log(data)
+    if (response.status == 404) {
+        document.querySelector('.error').style.display = 'block'
+        document.querySelector('.weather').style.display = 'none'
+    } else {
+        var data = await response.json()
 
-    document.querySelector('.city').innerHTML = data.name
+        // console.log(data)
 
-    document.querySelector('.temp').innerHTML = Math.round(data.main.temp) + '°C'
+        document.querySelector('.city').innerHTML = data.name
+        document.querySelector('.temp').innerHTML = Math.round(data.main.temp) + '°c'
+        document.querySelector('.humidity').innerHTML = data.main.humidity + '%'
+        document.querySelector('.wind').innerHTML = data.wind.speed + ' km/h'
 
-    document.querySelector('humidity').innerHTML = data.main.humidity + '%'
+        if (data.weather[0].main == 'Clouds') {
+            weatherIcon.src = 'images/clouds.png'
+        }
+        else if (data.weather[0].main == 'Clear') {
+            weatherIcon.src = 'images/clear.png'
+        }
+        else if (data.weather[0].main == 'Rain') {
+            weatherIcon.src = 'images/rain.png'
+        }
+        else if (data.weather[0].main == 'Drizzle') {
+            weatherIcon.src = 'images/drizzle.png'
+        }
+        else if (data.weather[0].main == 'Mist') {
+            weatherIcon.src = 'images/mist.png'
+        }
 
-    document.querySelector('.wind').innerHTML = data.wind.speed + 'km/h'
+        document.querySelector('.weather').style.display = 'block'
+        document.querySelector('.error').style.display = 'none'
+    }
 
 }
 
 searchBtn.addEventListener('click', () => {
     checkWeather(searchBox.value)
 })
+
+
 
 
 
